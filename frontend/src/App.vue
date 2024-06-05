94ec50acc5037daed87e3ae828941345d14cad51
eli-bans <elikembansah@gmail.com>
2024-06-05 07:07:45 +0000

    Removed HelloWorld.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,7 +15,6 @@ import Navbar from './components/Navbar.vue';
 import IntroSection from './components/IntroSection.vue';
 import OfferSection from './components/OfferSection.vue';
 import TutorSection from './components/TutorSection.vue';
-import JourneySecton from './components/JourneySection.vue';
 import JourneySection from './components/JourneySection.vue';
 
 export default defineComponent({
